eb05d0f207301365b73c68d3811534150959669e
Alexander Argueta <ale.kcj1998@gmail.com>
2020-11-11 21:56:34 -0600

    Agregando validacion js para desplegar/ocultar menu

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,15 @@
 
+
+var cont = 1;
+const menu=document.getElementById("menuitems")
+const button=document.getElementById("btnmenu")
+button.addEventListener('click', () => {
+   if (cont==1){
+    menu.style.display = "block"
+    cont = 0;
+   }else{
+       cont = 1;
+       menu.style.display = "none"
+   }
+  
+})
